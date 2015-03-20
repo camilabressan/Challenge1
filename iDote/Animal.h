73af3600754be4d2daac57b9ab53a3755ae06f3b
Eduardo Santi <eduardo.senacrs@gmail.com>
2015-03-20 16:39:21 -0300

    prototipo de tela
    alterações na tela de cadastro de animais

---
+++ iDote/Animal.h
@@ -0,0 +1,20 @@
+//
+//  Animal.h
+//  iDote
+//
+//  Created by Eduardo Santi on 20/03/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animal : NSObject
+
+@property NSString *nome;
+@property NSInteger idade;
+@property NSString *porte;
+@property NSString *genero;
+@property NSString *descricao;
+
+
+@end
---
+++ iDote/Animal.m
@@ -0,0 +1,13 @@
+//
+//  Animal.m
+//  iDote
+//
+//  Created by Eduardo Santi on 20/03/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@end
