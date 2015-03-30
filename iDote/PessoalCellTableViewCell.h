c968c0289f690258ee3c3d489ea641054c018744
Eduardo Santi <eduardo.senacrs@gmail.com>
2015-03-30 15:49:45 -0300

    tela pessoal ja exibe dados pessoais, pets e eventos

---
+++ iDote/PessoalCellTableViewCell.h
@@ -0,0 +1,22 @@
+//
+//  PessoalCellTableViewCell.h
+//  iDote
+//
+//  Created by Eduardo Santi on 30/03/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Animal.h"
+#import "Evento.h"
+
+@interface PessoalCellTableViewCell : UITableViewCell
+
+@property Animal *animal;
+@property Evento *event;
+
+@property (weak, nonatomic) IBOutlet UILabel *cellName;
+
+
+
+@end
