0236259ab12831ae0095710669537a8c9becc7ad
Jonathan Andrade <jonathan_andrade91@hotmail.com>
2015-03-30 16:41:59 -0300

    Show Event Details: time zone DONE!

--- iDote/DetailEventViewController.m
+++ iDote/DetailEventViewController.m
@@ -20,7 +20,9 @@
     _detailNameEvent.text = _ev.nomeEvento;
     _detailAddress.text = _ev.endereco;    
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSTimeZone *timeZone = [NSTimeZone localTimeZone];
     [dateFormatter setDateFormat:@"dd-MM-yyyy HH:mm"];
+    [dateFormatter setTimeZone:timeZone];
     _detailDate.text = [dateFormatter stringFromDate:_ev.date];
     _detailHour.text = _ev.horario;
     _detailDescription.text = _ev.descricao;
