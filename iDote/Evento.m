f54248d09956c8d2ab5c0b00617244f74e1e6058
Jonathan Andrade <jonathan_andrade91@hotmail.com>
2015-03-31 14:09:01 -0300

    Create new event: now is a FXForm

--- iDote/Evento.m
+++ iDote/Evento.m
@@ -10,6 +10,22 @@
 
 @implementation Evento
 
+- (NSArray *)fields
+{
+    return @[@{FXFormFieldKey: @"nomeEvento",
+               FXFormFieldTitle: @"Nome do Evento",
+               FXFormFieldHeader: @"DADOS"},
+             @{FXFormFieldKey: @"endereco",
+               FXFormFieldTitle: @"Endereço"},
+             @{FXFormFieldKey: @"date",
+               FXFormFieldTitle: @"Data",
+               FXFormFieldType: FXFormFieldTypeDateTime},
+             @{FXFormFieldTitle: @"Descrição",
+               FXFormFieldKey: @"descricao",
+               FXFormFieldType: FXFormFieldTypeLongText,
+               FXFormFieldPlaceholder: @"Insira aqui descrições gerais do animal..."}];
+}
+
 - (void)save {
     PFObject *object = [PFObject objectWithClassName:@"Event"];
     object[@"nome"] = _nomeEvento;
