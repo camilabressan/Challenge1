b3eb56ceb201508ea936c2ead1da0ec4c1056618
Tainara Specht <tay.specht@gmail.com>
2015-03-31 10:02:32 -0300

    Code for constraint bottom - keyboard goes up with register institution view;

--- iDote/RegisterInstitutionViewController.h
+++ iDote/RegisterInstitutionViewController.h
@@ -25,4 +25,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *addInstPic;
 @property (weak, nonatomic) IBOutlet VMaskTextField *maskInstitutionPhone;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintBottom;
+
+
+
 @end
--- iDote/RegisterInstitutionViewController.m
+++ iDote/RegisterInstitutionViewController.m
@@ -13,13 +13,23 @@
 @interface RegisterInstitutionViewController() <MFMailComposeViewControllerDelegate>
 
 @end
-@implementation RegisterInstitutionViewController
-    Institution *institution;
+@implementation RegisterInstitutionViewController{
     
+    Institution *institution;
+    CGFloat _initialConstant;
+
+}
+
+static CGFloat _keyboardHeightOffset = 15.0f;
+
 
 - (void)viewDidLoad{
+    [super viewDidLoad];
     //telephone with area code
     self.maskInstitutionPhone.mask = @"(##) ####-#####";
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
 }
 
 //method for replacing the telephone field with mask;
@@ -137,6 +147,46 @@
         [self cadastrarInst];
         return YES;
 }
+
+- (IBAction)dismissKeyboard {
+    
+    // This method will resign all responders, dropping the keyboard.
+    [self.view endEditing:YES];
+    
+}
+
+- (void)keyboardWillShow:(NSNotification*)notification {
+    
+    // Getting the keyboard frame and animation duration.
+    CGRect keyboardFrame = [notification.userInfo[UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    NSTimeInterval keyboardAnimationDuration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    if (!_initialConstant) {
+        _initialConstant = _constraintBottom.constant;
+    }
+    
+    // If screen can fit everything, leave the constant untouched.
+    _constraintBottom.constant = MAX(keyboardFrame.size.height + _keyboardHeightOffset, _initialConstant);
+    [UIView animateWithDuration:keyboardAnimationDuration animations:^{
+        // This method will automatically animate all views to satisfy new constants.
+        [self.view layoutIfNeeded];
+    }];
+    
+}
+
+- (void)keyboardWillHide:(NSNotification*)notification {
+    
+    // Getting the keyboard frame and animation duration.
+    NSTimeInterval keyboardAnimationDuration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    // Putting everything back to place.
+    _constraintBottom.constant = _initialConstant;
+    [UIView animateWithDuration:keyboardAnimationDuration animations:^{
+        [self.view layoutIfNeeded];
+    }];
+    
+}
+
 
 - (IBAction)clickOnBackground:(id)sender {
     [self.view endEditing:YES];
