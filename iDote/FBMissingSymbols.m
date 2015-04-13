bd0d86e70e19fe79d30a190a6555498f6e4f71e4
Adriano Soares <gsoares.adriano@gmail.com>
2015-04-13 19:22:26 -0300

    Fixed Facebook Framework issues

---
+++ iDote/FBMissingSymbols.m
@@ -0,0 +1,34 @@
+//
+//  FBMissingSymbols.m
+//  iDote
+//
+//  Created by Adriano Soares on 13/04/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+NSString *FBTokenInformationExpirationDateKey = @"";
+NSString *FBTokenInformationTokenKey = @"";
+NSString *FBTokenInformationUserFBIDKey = @"";
+@interface FBAppCall:NSObject
+@end
+@implementation FBAppCall
+@end
+@interface FBRequest:NSObject
+@end
+@implementation FBRequest
+@end
+@interface FBSession:NSObject
+@end
+@implementation FBSession
+@end
+@interface FBSessionTokenCaching:NSObject
+@end
+@implementation FBSessionTokenCaching
+@end
+@interface FBSessionTokenCachingStrategy:NSObject
+@end
+@implementation FBSessionTokenCachingStrategy
+@end
