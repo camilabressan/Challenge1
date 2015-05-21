dc03c28f77019f2dd6dda34f9fdc9ac4742ebf7c
Jonathan Andrade <jonathan_andrade91@hotmail.com>
2015-05-21 16:31:28 -0300

    Apple Watch target added

---
+++ Pet's Bay WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Pet's Bay WatchKit Extension
+//
+//  Created by Jonathan Andrade on 21/05/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
